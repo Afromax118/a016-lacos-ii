b1cd6cf9ebc5e2b6c6460e2d13674d5b2a3faa74
Gabriel Max <gabrielmaxcerqueira@gmail.com>
2022-08-18 13:08:35 -0300

    Done

---
+++ exercicio2/script.js
@@ -0,0 +1,31 @@
+// # Exercício 2
+
+// Crie um programa que peça um input de número para o usuário. Com este número, o código deve imprimir a **tabuada** do número, de 1 a 10.
+
+// **Utilize o for...in para resolver**
+
+// Exemplo com entrada **`7`**:
+// ```jsx
+// 7
+// 14
+// 21
+// 28
+// 35
+// 42
+// 49
+// 56
+// 63
+// 70
+// ```
+
+const numero = Number(prompt("Digite um número para a tabuada:"))
+
+let tabuada = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let resultado = 0
+
+for(i in tabuada){
+    resultado = numero*tabuada[i] 
+    console.log(resultado)
+    i++    
+}
